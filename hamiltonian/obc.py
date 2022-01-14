1f2e66f082e5791a14f1f2bf4ee619701d9f9d7f
Sourav kumar Dash <33924763+srv96@users.noreply.github.com>
2022-01-14 22:06:40 +0530

    Update obc.py
    code updated

--- hamiltonian/obc.py
+++ hamiltonian/obc.py
@@ -1,7 +1,5 @@
-# -*- coding: utf-8 -*-
 """
 Spyder Editor
-
 This is a temporary script file.
 """
 
@@ -56,26 +54,70 @@ class hamiltonian:
         return e_matrix
 
 
-
-
 if __name__ == "__main__":
     b = 0.0
     d = 1
     l = 1
     M = 3
-    kx = 0
-    ky = 0
-    n = 800
+    kx = np.arange(-np.pi, np.pi, 1)
+    ky = np.arange(-np.pi, np.pi, 1)
+    n = 10
 
     H = hamiltonian(b, d, l, M)
 
-    result = H.get_matrix(n, kx, ky)
+    w_real_all, w_imag_all, v4_all = [], [], []
+    counter, star_count = 0, 0
 
-    print(result)
+    for i in kx:
+        print("\nkx = ", counter, " started")
+        counter += 1
+        star_count = 101
 
-w, v = LA.eig(H.get_matrix(n, kx, ky))
-w1 = np.real(w)
-w2 = np.imag(w)
+        for j in ky:
+            if star_count < 100:
+                print("*", end="")
+                star_count += 1
+            else:
+                print("\n*", end="")
+                star_count += 1
 
-plt.scatter(w1, w2, s=10)
-plt.plot
+            w, v = LA.eig(H.get_matrix(n, i, j))
+            v4 = []
+            for vi in v:
+                vt = vi.reshape(1, vi.shape[0])
+                v4.append(np.square(np.real(vt.dot(vt.conjugate().T)[0])[0]))
+            v4 = np.array(v4)
+            w_real_all.append(np.array([c.real for c in w]))
+            w_imag_all.append(np.array([c.imag for c in w]))
+            v4_all.append(v4)
+
+    w_real_all = np.array(w_real_all).flatten()
+    w_imag_all = np.array(w_imag_all).flatten()
+    v4_all = np.array(v4_all).flatten()
+
+    # result = H.get_matrix(n, kx, ky)
+
+    # print(result)
+
+# w, v = LA.eig(H.get_matrix(n, kx, ky))
+# w1 = np.real(w)
+# w2 = np.imag(w)
+#
+# v4 = []
+# for vi in v:
+#     vt = vi.reshape(1, vi.shape[0])
+#     v4.append(np.square(np.real(vt.dot(vt.conjugate().T)[0])[0]))
+# v4 = np.array(v4)
+#
+# print(w1.shape, w2.shape, v4.shape)
+#
+#
+# plt.show()
+
+plt.figure(figsize=(27, 18), dpi=1000)
+plt.title("dispersion plot")
+plt.xlabel('> ===== imaginary  ====> ', fontsize=12)
+plt.ylabel('> ===== real  ====> ', fontsize=12)
+plt.scatter(w_imag_all, w_real_all, marker='.', s=0.01, linewidths=4, c=v4_all, cmap=plt.cm.coolwarm)
+plt.savefig('filename.png', dpi=1000)
+plt.show()
